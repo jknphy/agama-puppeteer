96445857e1ace2b514900ff79b798204e1395e34
jknphy <jeriveramoya@suse.com>
2024-06-10 14:07:03 +0200

    blabl

--- tests/test.js
+++ tests/test.js
@@ -103,7 +103,7 @@ describe("Agama test", function () {
     }
   });
 
-  it("should have Agama page title mega ok", async function () {
+  it("should have Agama page title super dupi dupi", async function () {
     expect(await page.title()).to.eql("Agama");
   });
 
